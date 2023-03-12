6a90924f4eff2be7e674344ea78c1fd35230f42d
andersonzero0 <programadorzero@hotmail.com>
2023-03-12 13:11:57 -0300

    bug

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ const PORT = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-    /* res.render("pages/home"); */
-    res.send("Hello World");
+    res.render("pages/home.ejs");
 })
 
 app.listen(PORT, () => {
