38af203d4f44056d9344c95500c9da8886a7f5a2
Anderson Viana <92057959+andersonzero0@users.noreply.github.com>
2023-05-23 21:36:03 -0300

    update

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react"
+import { Moon } from "lucide-react"; 
+import './style.css';
+
+export function Header() {
+
+    const [user, setUser] = useState({})
+
+    function handleTheme() {
+
+        
+        
+    }
+
+    useEffect(() => {
+
+        fetch("https://api.github.com/users/andersonzero0")
+        .then(response => response.json())
+        .then(data => {
+
+            let name_data = data.name
+            name_data = name_data.toUpperCase();
+
+            setUser({
+                name: name_data,
+                avatar: data.avatar_url
+            })
+            
+        })
+        
+    }, [])
+
+    return (
+        <header>
+
+            <div>
+                <h2>WEB DEVELOPER</h2>
+
+                <img className="gif_github" src="./github_mini.gif" alt="" />
+            </div>
+
+            <div className="box_user">
+
+                <p>{user.name}</p>
+                <img src={user.avatar} alt="" />
+
+                <div onClick={handleTheme} className="box_moon">
+                    <Moon/>
+                </div>
+
+            </div>
+            
+        </header>
+    )
+    
+}
