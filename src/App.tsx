6b1f33a83a1eb901fd5bfcc3afb253a180fe6db4
Anderson Viana <anderson11viana@gmail.com>
2025-01-20 20:54:44 -0300

    Refactor hero component to add AboutMe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { AboutMe } from "./components/about-me";
 import { Header } from "./components/header";
 
 
 const App = () => {
 
   return (
-    <main className="container h-screen m-auto p-2 dark">
+    <main className="container h-screen m-auto p-2 dark font-plex-mono flex flex-col gap-10">
       <Header />
+      <AboutMe />
     </main>
   );
 };
---
+++ src/components/about-me.tsx
@@ -0,0 +1,15 @@
+export function AboutMe() {
+  return (
+    <div>
+      <h2 className="font-retro mb-2">Sobre mim</h2>
+
+      <div className="flex flex-col gap-1">
+        <p className="text-sm">Desenvolvedor de Software com foco em Backend utilizando Node.js e NestJS. Trabalho no desenvolvimento de APIs escaláveis e integrações com bancos de dados como PostgreSQL, MongoDB e MySQL. Experiência com tecnologias para otimização de desempenho e gerenciamento de filas, como Kafka, que permitem processamento assíncrono em segundo plano.</p>
+
+        <p className="text-sm">Atualmente, estou cursando Ciência da Computação, onde aplico uma combinação de fundamentos teóricos e práticas modernas de desenvolvimento de software.</p>
+
+        <p className="text-sm">Apaixonado por criar soluções eficientes e escaláveis, busco contribuir para projetos desafiadores e inovadores, nos quais possa usar minhas habilidades para otimizar processos e melhorar a experiência do usuário.</p>
+      </div>
+    </div>
+  );
+}
