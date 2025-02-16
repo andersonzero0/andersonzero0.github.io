128125b89d03c1b396c1d3ce3050017ccf3ca44a
Anderson Viana <anderson11viana@gmail.com>
2025-02-16 16:43:02 -0300

    Refactor hero component to add AboutMe and Experiences components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { AboutMe } from "./components/about-me";
+import { Experiences } from "./components/experiences";
 import { Header } from "./components/header";
 
 
 const App = () => {
 
   return (
-    <main className="container h-screen m-auto p-2 dark font-plex-mono flex flex-col gap-10">
+    <main className="container h-screen m-auto p-2 dark font-plex-mono flex flex-col gap-6 overflow-auto">
       <Header />
       <AboutMe />
+      <Experiences />
     </main>
   );
 };
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,22 @@
+import { PrinterIcon } from "lucide-react";
+
 export function Header() {
+
+  function printPage() {
+    window.print();
+  }
+  
   return (
-    <header className="w-full flex flex-row h-16 items-center justify-between flex-wrap gap-4">
+    <header className="w-full flex flex-row min-h-16 items-center justify-between flex-wrap gap-4">
         <div className="h-full flex flex-row items-center gap-4">
-          <img src="http://github.com/andersonzero0.png" className="terminal-effect h-full" />
+          <img src="http://github.com/andersonzero0.png" className="rounded-2xl w-16 h-16" />
 
           <div>
-            <h1 className="font-retro">Anderson Viana</h1>
+            <h1 className="text-xl font-bold">Anderson Viana</h1>
             <p className="font-plex-mono">Desenvolvedor de Software</p>
+            <p>
+              ⭐️⭐️⭐️⭐️⭐️
+            </p>
           </div>
         </div>
 
@@ -17,7 +27,10 @@ export function Header() {
           <a href="">
             <img className="w-6" src="./linkedin.png" />
           </a>
+          <button onClick={printPage} className="flex justify-center items-center">
+            <PrinterIcon size={24}/>
+          </button>
         </div>
-      </header>
+    </header>
   )
 }
